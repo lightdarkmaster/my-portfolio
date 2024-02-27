38a739fb176c81660a717cbf03c4f4ade651a671
Christian Barbosa <christian.barbosa05222001@gmail.com>
2024-02-28 07:05:32 +0800

    Dragon Modification

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../css/Homepage.css'
+import dragonImage2 from '../assets/fantasy.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+
 
 function Homepage() {
   return (
     <div className="homepage">
+<div className="card">
+  <img src={dragonImage2}className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,45 +1,49 @@
 import React from 'react'
 import '../css/NavBar.css'
+import myImage from "../assets/dragon.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
 
 
 function NavBar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <div><img src="../assets/logo.jpeg"/></div>
+      <nav className=" nav navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <div><img src={myImage} className="logo"/></div>
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Me</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Me</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Links
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Github</a></li>
+            <li><a className="dropdown-item" href="#">Linkedin</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Facebook</a></li>
+            <li><a className="dropdown-item" href="#">Instagram</a></li>
+            <li><a className="dropdown-item" href="#">TikTok</a></li>
+            <li><a className="dropdown-item" href="#">Twitter</a></li>
+
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
         </li>
       </ul>
     </div>
